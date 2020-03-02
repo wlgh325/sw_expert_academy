c566643306b8a8a2a63118aa30bfaef95a00eb8a
Jiho <wlgh325@naver.com>
2020-03-02 21:53:19 +0900

    solve 디저트 카페

---
+++ 2105(디저트_카페)/Solution.java
@@ -0,0 +1,106 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Queue;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+
+
+class Solution {
+    static int max;
+    static int N;
+    static int[][] map;
+    // 0: 오른쪽 아래, 1: 왼쪽 아래, 2: 왼쪽 위, 3: 오른쪽 위
+    // 0, 1, 2, 3
+    // {0,1}, {1,2}, {2,3}, {3,0}
+    static int[][] xdir = {{1,1}, {1,-1}, {-1,-1}, {-1,1}};
+    static int[][] ydir = {{1,-1},{-1,-1},{-1,1},{1,1}};
+    static boolean[][] visited;
+    static int startX, startY;
+    static int startNum;
+    
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int testNum = Integer.parseInt(br.readLine());
+         
+        for(int test=1; test<=testNum; test++){
+            // 입력받기
+            N = Integer.parseInt(br.readLine());
+            map = new int[N][N];
+            for(int i=0; i<N; i++){
+                String[] temp = br.readLine().split(" ");
+                for(int j=0; j<N; j++){
+                    map[i][j] = Integer.parseInt(temp[j]);
+                }
+            }
+            
+            max = -1;
+            // 모든 위치에서 dfs 탐색
+            for(int i=0; i<N; i++){
+                for(int j=0; j<N; j++){
+                    visited = new boolean[N][N];
+                    ArrayList<Integer> list = new ArrayList<>();
+                    // 시작 위치와 시작점의 디저트 수 저장
+                    startX = i;
+                    startY = j;
+                    startNum = map[i][j];
+                    
+                    dfs(i,j, 0, list, 0, 0);
+                }
+            }
+            System.out.println("#" + test + " " + max);
+        }
+        br.close();
+    }
+
+    public static void dfs(int x, int y, int k, ArrayList<Integer> list, int dir, int rotate){
+        // 시작 위치에 도착 & 사각형 완성
+        if(k != 0 && startX == x && startY == y && rotate == 3){
+            int cafeNum = list.size();
+            max = max < cafeNum ? cafeNum : max;    // 최대값 갱신
+            return;
+        }
+        for(int i=0; i<2; i++){
+            // 시작점에서 오른쪽 아래로 내려가는 경우만 따짐으로써
+            // 중복 제거
+            if(k==0 && i == 1)
+                continue;
+            int dx = x + xdir[dir][i];
+            int dy = y + ydir[dir][i];
+            // 유효한 위치이면서 방문하지 않은 곳
+            if(isValidPosition(dx, dy) && !visited[dx][dy]){
+                // 이전에 방문했던 곳(시작 점 포함)의 값들과 달라야 함
+                // 시작점으로 돌아온 경우는 같을 수 밖에 없기 때문에 그 부분은 오른쪽 조건으로 예외처리
+                if(!isSame(list, map[dx][dy]) || ((dx == startX) && (dy == startY))){
+                    visited[dx][dy] = true;
+                    ArrayList<Integer> copy = new ArrayList<>();
+                    copy.addAll(list);
+                    copy.add(map[dx][dy]);
+
+                    
+                    if(rotate != (dir+i)%4)
+                        dfs(dx, dy, k+1, copy, (dir+i)%4, rotate+1);    // 같은 방향으로 가지 않기 때문에 방향을 꺾은 것으로 판단
+                    else
+                        dfs(dx, dy, k+1, copy, (dir+i)%4, rotate);  // 같은 방향으로 가는 경우는 꺾지 않은 것이므로 rotate 그대로 전달
+
+                    visited[dx][dy] = false;
+                }
+            }
+        }
+    }
+
+    public static boolean isValidPosition(int x, int y){
+        if(x < 0 || x >= N || y < 0 || y >= N) return false;
+        return true;
+    }
+
+    public static boolean isSame(ArrayList<Integer> list, int num){
+        for(int i=0; i<list.size(); i++){
+            if(num == list.get(i))
+                return true;
+        }
+        if(startNum == num) return true;
+        return false;
+    }
+}
